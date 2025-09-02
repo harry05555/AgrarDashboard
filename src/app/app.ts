20b64374db018e25a3f553fee0f84bfc1956b74c
Sebastian <93546847+harry05555@users.noreply.github.com>
2025-09-02 17:09:24 +0200

    Clean code for weather forecast and make css code global for the rest of comp.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { SearchingCities } from "./features/searching-cities/searching-cities";
-import { Location } from "./features/location/location";
+import { SearchingCities } from './features/searching-cities/searching-cities';
+import { Location } from './features/location/location';
+import { WeatherForecast } from './features/weather-forecast/weather-forecast';
 
 @Component({
   selector: 'app-root',
-  imports: [SearchingCities, Location],
+  imports: [SearchingCities, Location, WeatherForecast],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/features/weather-forecast/weather-forecast.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-forecast',
+  imports: [],
+  templateUrl: './weather-forecast.html',
+  styleUrl: './weather-forecast.css',
+})
+export class WeatherForecast {}
