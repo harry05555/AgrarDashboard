8631fc45c13ce75b61925ca8d4d065aa770e3486
Sebastian <93546847+harry05555@users.noreply.github.com>
2025-09-03 12:55:17 +0200

    Inputfield conect with controler

--- src/app/features/searching-cities/searching-cities.ts
+++ src/app/features/searching-cities/searching-cities.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SearchingDataInterface } from './searching-data-interface.data';
 
 @Component({
   selector: 'app-searching-cities',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './searching-cities.html',
   styleUrl: './searching-cities.css',
 })
-export class SearchingCities {}
+export class SearchingCities {
+  formData = signal<SearchingDataInterface>({
+    searchCitiy: '',
+  });
+
+  onSubmit() {
+    console.log('Form submitted:', this.formData());
+  }
+}
